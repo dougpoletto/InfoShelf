6f800b3d72b73c2974b84fb3a6e3fe38742841bd
dougpoletto <polettodoug@hotmail.com>
2024-08-24 16:01:51 -0300

    Injeção do contexto, service e repositório

--- InfoShelf.Server/Program.cs
+++ InfoShelf.Server/Program.cs
@@ -1,3 +1,10 @@
+using InfoShelf.Server.Domain.Interfaces.Respositories;
+using InfoShelf.Server.Domain.Interfaces.Services;
+using InfoShelf.Server.Domain.Services;
+using InfoShelf.Server.Infraestructure.Contexts;
+using InfoShelf.Server.Infraestructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar o DbContext
+builder.Services.AddDbContext<BookContext>(options =>
+    options.UseInMemoryDatabase("InfoShelfDatabase"));
+
+// Registrar o repositório como Scoped ou Transient
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<IBookService, BookService>();
 
 var app = builder.Build();
 
